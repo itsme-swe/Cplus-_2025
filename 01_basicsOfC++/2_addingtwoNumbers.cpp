445ff3beabce44083b1ccdedc18d957b33773087
itsme-swe <me.harshmehra07@gmail.com>
2024-12-18 12:39:52 +0530

    update

---
+++ 01_basicsOfC++/2_addingtwoNumbers.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  int a;
+  int b;
+
+  cout<< "Enter 2 numbers: ";
+
+  cin >> a >> b;
+
+  int c = a + b;
+
+  cout<<"The sum is "<< c;
+
+
+  return 0;
+}
