813a47685fed069f282d01f7988f71271f7196e3
itsme-swe <me.harshmehra07@gmail.com>
2025-01-11 22:20:01 +0530

    update

---
+++ 03_Pointers/2_PointerDemo.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  
+  int x = 10;
+
+  int y = 20;
+
+  int *P;
+
+  P = &x;
+
+  int* ptr = &y;
+
+  cout << "Value present inside x is: " << x << "\n";   // opt ›› 10
+
+  cout << "Value present inside &x is: " << &x << "\n"; // opt ›› 0x61ff08 address of variable x
+
+  cout << "Value present inside P is: " << P << "\n"; // opt ›› 0x61ff08 Pointer P is holding the address of variable x
+
+  cout << "Value present inside &P is: " << &P << "\n"; // opt›› Address of pointer P 0x61ff00
+
+  cout << "Value present inside *P is: " << *P << "\n"; // opt›› Dereferencing pointer P to get the value of variable x.
+
+  cout << endl;
+
+  cout << &y << "\n";
+
+  cout << ptr << "\n";
+
+  cout << *ptr << "\n";
+
+  return 0;
+}
---
+++ 03_Pointers/3_DereferencingOfArray.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  
+  int arr[] = {2, 4, 6, 8, 10};
+  
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  int* P = arr;
+
+  cout << *P << endl; // Pointing first element of an array
+
+  cout << endl;
+
+  for (int i = 0; i < n; i++) {
+    cout << i << ": " << *(P + i) << endl;
+  }
+
+  return 0;
+}
