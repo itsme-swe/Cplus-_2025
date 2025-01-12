9d83d366e9234f3b7d27339b8d09b4aac2d4fecb
itsme-swe <me.harshmehra07@gmail.com>
2025-01-12 19:17:59 +0530

    update

---
+++ 05_Strings/4_stringFunctions.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  
+  char s[50] = "I am Harsh";
+
+  cout << strlen(s) << " \n"; // o/p: 10 -- "strlen" is the function to find the length of string
+
+  string s1 = "Harsh";
+
+  string s2 = "Mehra";
+
+  string s3 = s1 + " " + s2;
+
+  cout << s3;
+
+
+
+  return 0;
+}
