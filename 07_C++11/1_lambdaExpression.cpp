0c895ffd7107f805d06d24b289604d2968554f83
itsme-swe <me.harshmehra07@gmail.com>
2025-01-16 12:14:21 +0530

    update

--- 07_C++11/1_lambdaExpression.cpp
+++ 07_C++11/1_lambdaExpression.cpp
@@ -13,5 +13,18 @@ int main()
   [](int x, int y)
   { cout << "Sum of x and y is " << (x + y); }(10, 20);
 
+  cout << endl;
+
+  int a = 10;
+  int b = 20;
+
+  [a, b]()
+  { cout << a << " " << b << endl; }(); // 10 20
+
+  cout << endl;
+
+  [&a, &b]()
+  { cout << ++a << " " << ++b << endl; }(); // 11 21
+
   return 0;
 }
